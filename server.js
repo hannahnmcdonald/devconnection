22df1076597ffb54810a2454c183274a46ac30c0
hannahnmcdonald <hannahnmcdonald@protonmail.com>
2021-12-16 20:16:43 -0500

    server.js update

--- server.js
+++ server.js
@@ -1,9 +1,10 @@
+// Express Server
 const express = require('express');
 
+// App 
 const app= express();
-
 app.get('/', (req, res) => res.send('API runnng'));
 
+// PORT
 const PORT = process.env.PORT || 3000;
-
-app.listen(PORT, () => console.log('Server started on port ${PORT}'));
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
