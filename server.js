68a290d89ee45dcdd57a06e971aebd9fd32b3305
hannahnmcdonald <hannahnmcdonald@protonmail.com>
2021-12-21 15:20:03 -0500

    connectdb to server

--- server.js
+++ server.js
@@ -1,10 +1,13 @@
 // Express Server
 const express = require('express');
-
+// Connect to MongoDB
+const connectDB = require('./config/db');
 // App 
 const app= express();
-app.get('/', (req, res) => res.send('API runnng'));
-
 // PORT
 const PORT = process.env.PORT || 3000;
+
+
+app.get('/', (req, res) => res.send('API runnng'));
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
+connectDB();
